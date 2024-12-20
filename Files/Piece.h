df2fb8605db265fa28258c2f6485d4a3cba38f26
IddoAm <amraniiddo@gmail.com>
2024-12-20 14:52:56 +0200

    Piece abtract class

--- Files/Piece.h
+++ Files/Piece.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <vector>
+#include "Position.h"
+
+class Piece {
+public:
+	Piece(bool white) : white(white) {}
+
+	virtual std::vector<Position> legalMoves(const Piece* board[8][8]) const = 0;
+protected:
+	bool white;
+};
