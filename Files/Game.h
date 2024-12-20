82fc791abc226bfb73662e8b211960b720893d6c
IddoAm <amraniiddo@gmail.com>
2024-12-20 15:24:04 +0200

    Game header

--- Files/Game.h
+++ Files/Game.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+
+#include "MoveStatus.h"
+
+#include "Board.h"
+#include "Position.h"
+
+class Game {
+public:
+	int move(std::string instruction);
+
+
+private:
+	int turn;
+
+	Position algebraicToPosition(std::string algebraic);
+};
