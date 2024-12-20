4808a5ddb7ec868b7c7190af6482adf2a28eec8f
IddoAm <amraniiddo@gmail.com>
2024-12-20 15:22:53 +0200

    Seperated enum move status

--- Files/Board.h
+++ Files/Board.h
@@ -5,22 +5,11 @@
 #include <string>
 #include <memory>
 
+#include "MoveStatus.h"
+
 #include "Position.h"
 #include "Piece.h"
 #include "King.h"
-
-enum class MoveStatus {
-    VALID_MOVE,
-    VALID_MOVE_CHECK_OPPONENT,
-    INVALID_MOVE_NO_PLAYER_PIECE,
-    INVALID_MOVE_SAME_PLAYER_PIECE,
-    INVALID_MOVE_CHECK_CURRENT,
-    INVALID_MOVE_ILLEGAL_INDEX,
-    INVALID_MOVE_ILLEGAL_PIECE_MOVE,
-    INVALID_MOVE_SAME_SQUARE,
-    VALID_MOVE_CHECKMATE
-};
-
 
 class Board {
 public:
