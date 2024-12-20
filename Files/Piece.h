79fb959c403c849a05ec84b7c5751f87969ccdeb
IddoAm <amraniiddo@gmail.com>
2024-12-20 15:23:52 +0200

    Added GetCharRepresentation() abstact to Piece.h

--- Files/Piece.h
+++ Files/Piece.h
@@ -7,6 +7,7 @@ public:
 	Piece(bool white) : white(white) {}
 
 	virtual std::vector<Position> legalMoves(const Piece* board[8][8]) const = 0;
+	virtual	char GetCharRepresentation() const = 0;
 protected:
 	bool white;
 };
