9bcf23d873d132c65204fb9c8abfc82b9858ab66
hilleld200 <hilleld2008@gmail.com>
2024-12-26 15:18:16 +0200

    working defult View

--- Files/IGameView.h
+++ Files/IGameView.h
@@ -3,7 +3,7 @@
 
 class IGameView {
 	public:
-	virtual ~IGameView();
+	virtual ~IGameView() = 0;
 	virtual void sendMessageToView(const std::string& msg) = 0;
 	virtual std::string getUserInput() = 0;
 };
