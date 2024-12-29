d8c1f744aaaecbe758bcf4a33282b6da733fc6d9
hilleld200 <hilleld2008@gmail.com>
2024-12-29 16:45:42 +0200

    fix the king movment

--- Files/King.cpp
+++ Files/King.cpp
@@ -8,47 +8,20 @@ King::King(bool white)
 
 std::vector<Position> King::legalMoves(const std::unique_ptr<Piece> board[8][8], const Position& pos) const
 {
-    bool cantMove[8][8] = { 0 };
-    //checking where the king can move
-    for (size_t i = 0; i < 8; i++)
-    {
-        for (size_t j = 0; j < 8; j++)
-		{
-            if (board[i][j] != nullptr)
-            {
-                //cant move because of a team member
-                if (board[i][j]->isWhite() == this->isWhite())
-				{
-					cantMove[i][j] = true;
-				}
-                //cant move because of checkmate if move
-                else
-                {
-                    std::vector<Position> moves = board[i][j]->warning_moves(board, Position(i, j));
-
-                    for (size_t k = 0; k < moves.size(); k++)
-					{
-						cantMove[moves[k].x][moves[k].y] = true;
-					}
-                }
-            }
-		}
-    }
-    
-    // now check the king moves
-    std::vector<Position> moves;
-    int x = pos.x, y = pos.y;
-    for (size_t i = 0; i < 8; i++)
-    {
-        if (x + _posibleMoves[i][0] < 0 || x + _posibleMoves[i][0] > 7 || y + _posibleMoves[i][1] < 0 || y + _posibleMoves[i][1] > 7)
+	std::vector<Position> moves;
+	
+	for (size_t i = 0; i < 8; i++)
+	{
+		if (pos.x + _posibleMoves[i][0] < 0 || pos.x + _posibleMoves[i][0] > 7 || pos.y + _posibleMoves[i][1] < 0 || pos.y + _posibleMoves[i][1] > 7)
 		{
 			continue;
 		}
-		if (!cantMove[x + _posibleMoves[i][0]][y + _posibleMoves[i][1]])
+		if (isPositionSafe(board, Position(pos.x + _posibleMoves[i][0], pos.y + _posibleMoves[i][1])) && (board[pos.x + _posibleMoves[i][0]][pos.y + _posibleMoves[i][1]] == nullptr || board[pos.x + _posibleMoves[i][0]][pos.y + _posibleMoves[i][1]]->isWhite() != this->isWhite()))
 		{
-			moves.push_back(Position(x + _posibleMoves[i][0], y + _posibleMoves[i][1]));
+			moves.push_back(Position(pos.x + _posibleMoves[i][0], pos.y + _posibleMoves[i][1]));
 		}
-    }
+	}
+
     return moves;
 }
 
